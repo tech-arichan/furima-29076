676875643799d6b451c394da898b0ad252cd4aa5
tech-arichan <arihiro2940@gmail.com>
2020-11-01 10:11:38 +0900

    ルーティング・コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit]
   
   def index
     @items = Item.order("created_at DESC")
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
   end
 
 
