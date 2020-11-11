84d85cf29330881de57538e0ac3c59a88f9f9b2c
tech-arichan <arihiro2940@gmail.com>
2020-11-11 12:55:37 +0900

    viewの作成

---
+++ app/controllers/item_purchases_controller.rb
@@ -0,0 +1,8 @@
+class ItemPurchasesController < ApplicationController
+  def index
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'item_purchases/index'
   devise_for :users
   root to: "items#index"
   resources :items
+  resources :item_purchases, only:[:index, :create]
 end
