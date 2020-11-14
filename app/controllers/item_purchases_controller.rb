28b40481f05dc8e21f627ae38fdb94c78d8f64b6
tech-arichan <arihiro2940@gmail.com>
2020-11-14 18:24:03 +0900

    複数テーブルへの保存

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,2 +1,28 @@
 class ItemPurchasesController < ApplicationController
+  before_action :set_item, only:[:index, :create]
+
+  def index
+    @item_purchase = Purchase.new
+  end
+
+  def create
+    @item_purchase = Purchase.new(item_purchase_params)
+    if @item_purchase.valid?
+      @item_purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def item_purchase_params
+    params.require(:purchase).permit(:postal_code, :city, :prefecture_id, :house_number, :building_number, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
