8a248f4e38725d309f082144bb21d28b32fc2948
tech-arichan <arihiro2940@gmail.com>
2020-11-15 15:06:14 +0900

    view　条件分岐　再修正

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     prefecture_id {1}
     city {'小城市'}
     house_number {'123'}
-    building_name {''}
+    building_name {'キャナルシティ'}
     phone_number {'12345678910'}
     token {"tok_abcdefghijk00000000000000000"}
   end
