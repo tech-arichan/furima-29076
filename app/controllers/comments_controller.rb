0dd5e4f00ed9b6fa84126799b6057a9e2aad46f2
tech-arichan <arihiro2940@gmail.com>
2020-12-06 15:31:06 +0900

    controller,view 編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,23 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.save
+    redirect_to item_path(@comment.item.id)
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @item = Item.find(params[:id])
     @comment.destroy
+    # if @comment.user_id != current_user.id
+    #   flash[:alert] = "その操作はできません"
+    #   redirect_to root_path
+    # end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commet, :item_id).merge(user_id: current_user.id)
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
 end
