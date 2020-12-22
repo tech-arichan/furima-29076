dabce35d3d408ff14a16de73fb82b07e6483b41b
tech-arichan <arihiro2940@gmail.com>
2020-12-22 12:34:49 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in?
-      #  && current_user.id == @item.user_id
+    if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
     else
